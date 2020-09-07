b1fc38bd7d363f9d32a949e0ea6e7c3de74360c3
Kurisutofu1990 <christoph.moeller90@gmx.net>
2020-09-07 12:38:14 +0900

    Updates

---
+++ precache-manifest.52d58f170288942eb591f411ee1aeb54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc68c39826b59c0c761e6e127fc53a4e",
+    "url": "/robofriendrebuild/index.html"
+  },
+  {
+    "revision": "b0e1e2713745c676ea95",
+    "url": "/robofriendrebuild/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3cb11d82ca41d2d16e86",
+    "url": "/robofriendrebuild/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "b0e1e2713745c676ea95",
+    "url": "/robofriendrebuild/static/js/2.bef8b9ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriendrebuild/static/js/2.bef8b9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb11d82ca41d2d16e86",
+    "url": "/robofriendrebuild/static/js/main.88d8841d.chunk.js"
+  },
+  {
+    "revision": "eb0121ec01f329db7bbe",
+    "url": "/robofriendrebuild/static/js/runtime-main.317653df.js"
+  }
+]);
